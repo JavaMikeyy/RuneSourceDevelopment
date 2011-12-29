127f17d82cafbd2521b84e652bbd5e3235ec58a8
Michael Sasse <metallic_mike@yahoo.com>
2011-12-29 09:45:51 -0500

    File

--- Build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-@title [%date% %time%] Build
-:compile
-mkdir bin
-echo [%date% %time%]: Compiling source code...
-java -jar libs/javac++.jar src libs bin
-echo [%date% %time%]: Done!
-pause
--- Initialize.bat
+++
@@ -1,4 +0,0 @@
-@echo Off
-title Server
-java -Xmx512m -XX:+DisableExplicitGC -noverify -cp bin;libs/* com.rs2.Server 127.0.0.1 43594 600
-pause
